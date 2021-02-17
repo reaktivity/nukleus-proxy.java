97e290a1be645f3d846e5000c78aa2fc789800cb
John Fallows <john.r.fallows@gmail.com>
2021-02-16 17:51:16 -0800

    Increase coverage

--- src/test/java/org/reaktivity/nukleus/proxy/internal/config/ProxyMatcherTest.java
+++ src/test/java/org/reaktivity/nukleus/proxy/internal/config/ProxyMatcherTest.java
@@ -16,6 +16,7 @@
 package org.reaktivity.nukleus.proxy.internal.config;
 
 import static org.agrona.BitUtil.fromHex;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.reaktivity.nukleus.proxy.internal.types.ProxyAddressProtocol.STREAM;
 
@@ -78,6 +79,24 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithTransport()
+    {
+        ProxyCondition condition = new ProxyCondition("datagram", null, null, null, null);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithFamily()
     {
         ProxyCondition condition = new ProxyCondition(null, "inet4", null, null, null);
@@ -93,6 +112,24 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithFamily()
+    {
+        ProxyCondition condition = new ProxyCondition(null, "inet6", null, null, null);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     @Test
@@ -114,6 +151,24 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithSource()
+    {
+        ProxyCondition condition = new ProxyCondition(null, null, new ProxyAddress("127.0.0.0/24", 32768), null, null);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithDestination()
     {
         ProxyCondition condition = new ProxyCondition(null, null, null, new ProxyAddress("192.168.0.0/24", 443), null);
@@ -129,6 +184,24 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithDestination()
+    {
+        ProxyCondition condition = new ProxyCondition(null, null, null, new ProxyAddress("127.0.0.0/24", 443), null);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     @Test
@@ -152,6 +225,26 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithAlpn()
+    {
+        ProxyInfo info = new ProxyInfo("http/1.1", null, null, null, null);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.alpn("echo"))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithAuthority()
     {
         ProxyInfo info = new ProxyInfo(null, "example.com", null, null, null);
@@ -169,6 +262,26 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithAuthority()
+    {
+        ProxyInfo info = new ProxyInfo(null, "example.net", null, null, null);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.authority("example.com"))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     @Test
@@ -192,6 +305,26 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithIdentity()
+    {
+        ProxyInfo info = new ProxyInfo(null, null, fromHex("87654321"), null, null);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.identity(v -> v.value(x -> x.set(fromHex("12345678")))))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithNamespace()
     {
         ProxyInfo info = new ProxyInfo(null, null, null, "example", null);
@@ -209,6 +342,26 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithNamespace()
+    {
+        ProxyInfo info = new ProxyInfo(null, null, null, "mismatch", null);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.namespace("example"))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     @Test
@@ -233,6 +386,27 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithSecureVersion()
+    {
+        ProxySecureInfo secureInfo = new ProxySecureInfo("TLSv1.2", null, null, null, null);
+        ProxyInfo info = new ProxyInfo(null, null, null, null, secureInfo);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.secure(s -> s.protocol("TLSv1.3")))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithSecureCipher()
     {
         ProxySecureInfo secureInfo = new ProxySecureInfo(null, "ECDHE-RSA-AES128-GCM-SHA256", null, null, null);
@@ -251,6 +425,27 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithSecureCipher()
+    {
+        ProxySecureInfo secureInfo = new ProxySecureInfo(null, "ECDH-ECDSA-AES256-GCM-SHA384", null, null, null);
+        ProxyInfo info = new ProxyInfo(null, null, null, null, secureInfo);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.secure(s -> s.cipher("ECDHE-RSA-AES128-GCM-SHA256")))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     @Test
@@ -275,6 +470,27 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithSecureKey()
+    {
+        ProxySecureInfo secureInfo = new ProxySecureInfo(null, null, "RSA1024", null, null);
+        ProxyInfo info = new ProxyInfo(null, null, null, null, secureInfo);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.secure(s -> s.key("RSA2048")))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithSecureName()
     {
         ProxySecureInfo secureInfo = new ProxySecureInfo(null, null, null, "name@domain", null);
@@ -296,6 +512,27 @@ public class ProxyMatcherTest
     }
 
     @Test
+    public void shouldNotMatchConditionWithSecureName()
+    {
+        ProxySecureInfo secureInfo = new ProxySecureInfo(null, null, null, "other@domain", null);
+        ProxyInfo info = new ProxyInfo(null, null, null, null, secureInfo);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.secure(s -> s.name("name@domain")))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
+    }
+
+    @Test
     public void shouldMatchConditionWithSecureSignature()
     {
         ProxySecureInfo secureInfo = new ProxySecureInfo(null, null, null, null, "SHA256");
@@ -314,6 +551,27 @@ public class ProxyMatcherTest
                 .build();
 
         assertTrue(matcher.matches(beginEx));
+    }
+
+    @Test
+    public void shouldNotMatchConditionWithSecureSignature()
+    {
+        ProxySecureInfo secureInfo = new ProxySecureInfo(null, null, null, null, "SHA384");
+        ProxyInfo info = new ProxyInfo(null, null, null, null, secureInfo);
+        ProxyCondition condition = new ProxyCondition(null, null, null, null, info);
+        ProxyMatcher matcher = new ProxyMatcher(condition);
+
+        ProxyBeginExFW beginEx = builder
+                .address(a -> a.inet4(i -> i
+                        .protocol(p -> p.set(STREAM))
+                        .source(s -> s.set(resolveHost("192.168.0.1")))
+                        .destination(d -> d.set(resolveHost("192.168.0.254")))
+                        .sourcePort(32768)
+                        .destinationPort(443)))
+                .infosItem(i -> i.secure(s -> s.signature("SHA256")))
+                .build();
+
+        assertFalse(matcher.matches(beginEx));
     }
 
     private static byte[] resolveHost(
